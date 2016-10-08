9a467fd0b3690df5b0254f762421fd9700fd1107
sieren <matthias@s-r-n.de>
2016-10-08 12:03:01 +0200

    Update gitconfig

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,4 +4,5 @@ ln -s ~/dotfiles/.vim/ ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
