b8a435ba033f9942ced0d6b3496ac405c74735bb
sieren <matthias@s-r-n.de>
2018-08-29 11:27:48 +0200

    update

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/Users/mfk/dotfiles/.vim/
--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -2,6 +2,7 @@
 
 ln -s ~/dotfiles/.vim/ ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/zgen ~/.zgen
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
